f2b65dde3751f951623cf65c8b9123d7e711cc03
RyhanRyuji <ryhanwahid@gmail.com>
2022-09-30 08:50:30 +0400

    Updates

---
+++ precache-manifest.d3bdf1e882036006a5717e889b93606b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4b33c3d036301ed38b8",
+    "url": "/Portfolio/static/css/main.d71bea29.chunk.css"
+  },
+  {
+    "revision": "d4b33c3d036301ed38b8",
+    "url": "/Portfolio/static/js/main.d4b33c3d.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/Portfolio/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "b49c3f4b9e0c77f15d4e",
+    "url": "/Portfolio/static/js/2.b49c3f4b.chunk.js"
+  },
+  {
+    "revision": "a3086c6906860bc0b730",
+    "url": "/Portfolio/static/js/runtime~main.a3086c69.js"
+  },
+  {
+    "revision": "ba0f12940c3f28e684f2a9d93dac6ce4",
+    "url": "/Portfolio/static/media/pp.ba0f1294.png"
+  },
+  {
+    "revision": "9293f5596b4b1c7dbee4f3a245ecef9a",
+    "url": "/Portfolio/index.html"
+  }
+];
